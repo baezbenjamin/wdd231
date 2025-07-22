980497ff85ea060f130105093adcbd155e792bd8
baezbenjamin <baezbenjamin03@gmail.com>
2025-07-22 09:45:02 -0300

    Header and Footer for the Chamber Site FInished

---
+++ scripts/tier-weather.js
@@ -0,0 +1,31 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=b5340ac44c6bf72997251db9a10cfb3a&units=metric';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", data.weather[0].main);
+    weatherIcon.setAttribute("height", "100");
+    weatherIcon.setAttribute("width", "auto");
+    captionDesc.textContent = `${desc}`;
+}
